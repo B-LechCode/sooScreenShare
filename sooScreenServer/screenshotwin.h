2d0e4426c4ac178fe4def3966f8345e39f8d2eea
Christian Schneider <schnaader@gmx.de>
2019-10-17 22:13:06 +0200

    License comment added to windows backend

--- sooScreenServer/screenshotwin.h
+++ sooScreenServer/screenshotwin.h
@@ -1,3 +1,7 @@
+//SooScreenServer by Christian Schneider (schnaader), 2019
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 #ifndef SCREENSHOTWIN_H
 #define SCREENSHOTWIN_H
 
