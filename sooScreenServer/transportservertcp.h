655ed15d00316f84e57b6ed11abfbc583c79b2cf
Simon Wezstein <simonwezsteinweb@web.de>
2019-10-26 14:40:43 +0200

    Done with doxy commenting

--- sooScreenServer/transportservertcp.h
+++ sooScreenServer/transportservertcp.h
@@ -18,84 +18,83 @@ namespace trans {
 }
 
 /**
- * @brief
- *
+ * @brief The transport server tcp class
+ * This class implements tcp communication based on the Qt library
  */
 class transportServerTCP :private QObject, public ItransportServer
 {
     Q_OBJECT
 public:
     /**
-     * @brief
+     * @brief The standard constructor
      *
      */
     transportServerTCP();
     /**
-     * @brief
+     * @brief The destructor
      *
      */
     virtual ~transportServerTCP();
     /**
-     * @brief
-     *
+     * @brief Init method
+     * This method is used to start the communication
      */
     virtual void init();
     /**
-     * @brief
-     *
-     * @param dat
-     * @param len
-     * @return int64_t
-     */
+    * @brief Send data over the connection
+    *
+    * @param dat Pointer to the data
+    * @param len Length of the data
+    * @return int64_t Return value of the socket, which is the number of sent bytes or < 0  when an error occured
+    */
     virtual int64_t send(const char* dat, int64_t len);
 private slots:
     /**
-     * @brief
-     *
+     * @brief The on new connection slot of the tcp server
+     * This is called if a client connects
      */
     void on_newConnection();
     /**
-     * @brief
-     *
+     * @brief The socked disconnected slot
+     * Initiates a restart of the tcp server (listening for connections)
      */
     void on_socketDisconnected();
 private:
-    QTcpServer m_srvr; /**< TODO: describe */
-    QTcpSocket* m_ptrSock = nullptr; /**< TODO: describe */
-    uint16_t   m_port; /**< TODO: describe */
-    QHostAddress m_interface; /**< TODO: describe */
+    QTcpServer m_srvr; /**< The tcp sercer */
+    QTcpSocket* m_ptrSock = nullptr; /**< Pointer to the connected socket */
+    uint16_t   m_port; /**< The port to listen on */
+    QHostAddress m_interface; /**< The network interface address to listen on */
     /**
-     * @brief
-     *
+     * @brief Ends all actions: Closes the socket, Stops the tcp server
+     * Call this prior tcp server restart
      */
     void end();
     /**
-     * @brief
+     * @brief Translates the parameters to the member variables
      *
      */
     void initParameters();
     /**
-     * @brief
-     *
-     * @param str
+     * @brief The new message event
+     * Will be called when a new message is available
+     * @param str The message
      */
     void notifyMessage(const char* str);
     /**
-     * @brief
-     *
-     * @param str
+     * @brief The new message event
+     * Will be called when a new message is available
+     * @param str The message
      */
     void notifyMessage(const std::string& str);
-
     /**
-     * @brief
+     * @brief The changed event of the underlying parameter map
      *
      */
     virtual void parameterMapChangedEvent();
     /**
-     * @brief
+     * @brief The changed event of a key/value pair
      *
-     * @param key
+     * @param key The key of the changed parameter
      */
     virtual void parameterChangedEvent(const std::string& key);
 
