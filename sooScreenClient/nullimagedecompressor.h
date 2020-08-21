7adf3b25959417e394a1675232dae59812e87471
Simon Wezstein <simonwezsteinweb@web.de>
2020-08-21 09:04:21 +0200

    Added passthru "decompression"

---
+++ sooScreenClient/nullimagedecompressor.h
@@ -0,0 +1,93 @@
+//sooScreenShare by Simon Wezstein (B-LechCode), 2019
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+#ifndef NULLIMAGEDECOMPRESSOR_H
+#define NULLIMAGEDECOMPRESSOR_H
+#include "iimagedecompressor.h"
+
+#include <utility>
+
+namespace comp
+{
+
+}
+
+/**
+ * @brief The jpeg image decompressor
+ *
+ */
+class nullImageDecompressor : public IImageDecompressor
+{
+public:
+    /**
+     * @brief The standard constructor
+     *
+     */
+    nullImageDecompressor();
+    /**
+     * @brief The destructor
+     *
+     */
+    virtual ~nullImageDecompressor();
+
+
+    /**
+     * @brief The method for image decomression.
+     *
+     * @param ptrDat Pointer to the raw data
+     * @param hdr    Header of the received data
+     * @param ok     Return reference for decompression status (True if ok)
+     * @return cv::Mat The decompressed image
+     */
+    inline virtual cv::Mat decompress(uint8_t* ptrDat,dataHeaderHandling::dataHeader hdr, bool& ok)
+    {
+        cv::Mat img;        
+        ok = decompressHelper(img,ptrDat,hdr);
+        return img;
+    }
+
+    /**
+    * @brief The method for image decomression.
+    *
+    * @param ptrDat Pointer to the raw data
+    * @param hdr    Header of the received data
+    * @return cv::Mat The decompressed image
+    */
+    inline virtual cv::Mat decompress(uint8_t* ptrDat,dataHeaderHandling::dataHeader hdr)
+    {        
+        cv::Mat img;
+        decompressHelper(img,ptrDat,hdr);
+        return img;
+    }
+
+private:
+    /**
+     * @brief Helper class for decompression
+     *
+     * @param img Image to be written
+     * @param ptrDat Pointer to the raw data
+     * @param hdr Header of the received data
+     * @return bool Decompression status (True if ok)
+     */
+    inline bool decompressHelper(cv::Mat& img,uint8_t* ptrDat,dataHeaderHandling::dataHeader hdr)
+    {
+        //if(hdr.length >= (hdr.width*hdr.height*))
+        img = cv::Mat(cv::Size(hdr.width, hdr.height),hdr.cvType,ptrDat);
+
+        return !img.empty();
+    }
+    /**
+     * @brief The changed event of the underlying parameter map
+     *
+     */
+    virtual void parameterMapChangedEvent();
+    /**
+     * @brief The changed event of a key/value pair
+     *
+     * @param key The key of the changed parameter
+     */
+    virtual void parameterChangedEvent(const std::string& key);
+};
+
+#endif // NULLIMAGEDECOMPRESSOR_H
