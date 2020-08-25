dbb192a3bb1825de0e00ef914e64dffbf2167e4b
Simon Wezstein <simonwezsteinweb@web.de>
2020-08-25 14:31:42 +0200

    Added a image consumer factory

---
+++ sooScreenClient/imageconsumerfactory.cpp
@@ -0,0 +1,7 @@
+//sooScreenShare by Simon Wezstein (B-LechCode), 2020
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+#include "imageconsumerfactory.h"
+
+std::vector<std::string> imageConsumerFactory::m_backends;
